84461bba3af3479ffcba76ae733e303789400a21
lubermudezz <bermudezluciana@hotmail.com>
2022-01-18 03:06:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenidos a la tienda de Minecraft By Jockerel</h1>
+    </div>
+  );
+}
+
+export default App;

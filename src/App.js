5b39be8090b193b92aeedff1f0d40babd318c860
lubermudezz <bermudezluciana@hotmail.com>
2022-01-31 15:46:28 -0300

    desafio 31/1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import {useState} from 'react';
 
-function App() {
+
+function App () {
+
+  const [count, setCount] = useState (0);
+  const [date, setDate] = useState (null);
+  const addCount = () => {
+    const date = new Date();
+    const hour = date.getHours ();
+    const minute = date.getMinutes();
+    const seconds = date.getSeconds ();
+    setDate({hour, minute, seconds})
+    setCount(count +1)
+  };
+  const discount = () => {setCount(count -1)}
+  const printDate= () => {
+    return <p> Time: {date.day}:{date.minute}:{date.seconds} </p>
+  }
   return (
-    <div className="App">
+    <div> 
+
+  <div className="App">
       <NavBar />
       <h1>Bienvenidos a la tienda de Minecraft By Jockerel</h1>
       <hr/>
       <ItemListContainer/>
-    </div>
+    
+</div>
 
+    <div class="button-count">
+    <h1>{count ? count : 'Not count'}</h1>
+    {date ? printDate() : ''}
+    <button onClick = {addCount}>Agregar item</button>
+    <button onClick = {discount}>Eliminar item</button>
+      </div>
+ </div>
   );
 }
 
